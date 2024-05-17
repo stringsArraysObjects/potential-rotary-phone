cdc8721efe1ec1b655b8e927859f643f6d766107
Chris <chtuff@gmail.com>
2024-05-17 19:53:41 -0400

    solution

---
+++ basic.sql
@@ -0,0 +1,10 @@
+-- create a simple SELECT statement that will return all 
+-- columns from the people table WHERE their age is over 50
+-- order by the age descending
+
+-- people table schema
+-- id
+-- name
+-- age
+
+select * FROM people WHERE age > 50 ORDER BY age DESC
