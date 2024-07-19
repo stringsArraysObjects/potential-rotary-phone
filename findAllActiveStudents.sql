2b9ea4c1e22c4e4b23c41bf85199ee104aa0b94e
Mason <chtuff@gmail.com>
2024-07-19 15:54:51 -0400

    solution

---
+++ findAllActiveStudents.sql
@@ -0,0 +1,8 @@
+-- Create a simple SELECT query to display student information of all ACTIVE students.
+
+-- TABLE STRUCTURE:
+
+-- students
+-- Id (integer)	FirstName (text)	LastName (text)	IsActive (boolean)
+
+SELECT * FROM students WHERE IsActive;
